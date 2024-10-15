c1944ef082a35f62a03b73536ca297712da5cb8e
Khairul Islam <khairul15-4102@diu.edu.bd>
2024-10-16 00:25:00 +0600

    added counter section with responsive

--- js/index.js
+++ js/index.js
@@ -11,3 +11,26 @@ closeSidebar.addEventListener('click', () => {
     mobileSidebar.classList.add('translate-x-full');
 });
 // <!-- JavaScript to control the popup menu end-->
+
+
+// Counter Start
+function countUp(elementId, targetNumber) {
+    let currentNumber = 0;
+    const interval = setInterval(() => {
+        if (currentNumber < targetNumber) {
+            currentNumber++;
+            document.getElementById(elementId).innerText = currentNumber;
+        } else {
+            clearInterval(interval);
+        }
+    }, 10); // Adjust the interval time for speed
+}
+
+// Initialize counters with target values
+window.onload = () => {
+    countUp('participants', 500);
+    countUp('subEvents', 3);
+    countUp('seminars', 5);
+    countUp('department', 53);
+};
+// Counter end
